94159d110bdbfbc8b6b3efa8a9237602695bc6b9
Prakash Chandra Palai <prakashchandrapalai91@gmail.com>
2022-06-30 09:16:28 +0530

    Adding Site directory check in site deploymnet file

--- shellscript/sites-hosting/deploy-sites.sh
+++ shellscript/sites-hosting/deploy-sites.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-function checkApcahe2Installed(){
-    dpkg -s apache2 >> install.log
+function checkAndInstallApcahe2Server(){
+    dpkg -s apache2 >> /dev/null
     local IS_APACHE2_INSTALLED=$?
     if [ $IS_APACHE2_INSTALLED -ne 0 ]
     then
@@ -15,7 +15,7 @@ function checkApcahe2Installed(){
         fi
     else
         echo "Apache2 is alreday installed , Hence chekcing Running Status"
-        local APACHE2_RUNNING_STATUS=$(sudo systemctl status apache2 | grep "Active:" | awk '{print $2}')
+        local APACHE2_RUNNING_STATUS=$(systemctl status apache2 | grep "Active:" | awk '{print $2}')
         if [ $APACHE2_RUNNING_STATUS != "active" ]
         then
             return 300
@@ -24,14 +24,43 @@ function checkApcahe2Installed(){
     return 0
 }
 
+function setupSiteDirectory(){
+    # If ZIP file location is not available
+    if [ ! -e $ZIP_LOC ]
+    then
+        return 400
+    fi
+
+    # If SITE_DIR is available in /var/www
+    if [ -d /var/www/$SITE_DIR ] 
+    then
+        TODAY=`date +%d%m%y%H%S`
+        sudo mv /var/www/$SITE_DIR /var/www/$SITE_DIR-$TODAY
+   else
+        # If SITE_DIR is not available in /var/www
+        sudo cp $ZIP_LOC /var/www/
+        sudo gunzip /var/www/$SITE_DIR.tar.gz
+        sudo tar -zxvf /var/www/$SITE_DIR.tar -C /var/www 
+        sudo chmod -R 755 /var/www/$SITE_DIR
+        sudo rm -rf /var/www/$SITE_DIR.tar
+    fi
+    return 0
+
+}
+
+#main function
+SITE_DIR=$1
+ZIP_LOC=$2
+DOMAIN_NM=$3
+
 ARGS=$#
 if [ $ARGS -ne 3 ]
 then
-    echo "ERROR: Please Pass sitedirectory, zipfilepath and domain name "
+    echo "ERROR:sitedirectory, zipfilepath and domain name are required"
  exit 100
  fi
 
-checkApcahe2Installed
+checkAndInstallApcahe2Server
 APACHE2_STATUS=$?
 if [ $APACHE2_STATUS -eq 200 ]
  then
@@ -40,8 +69,16 @@ if [ $APACHE2_STATUS -eq 200 ]
 fi
 if [ $APACHE2_STATUS -eq 300 ]
  then
-    echo "ERROR : Apache2 status is not runninng , Please check logs"
+    echo "ERROR : Apache2 status is not runninng , Before automating please restart the service ."
     exit 300
+fi
+
+setupSiteDirectory
+SITE_DIRECTORY_STATUS=$?
+if [ $SITE_DIRECTORY_STATUS -eq 400 ]
+then
+    echo "ERROR: Zip file location was wrong , Please make it correct."
+    exit 400
 fi
 
     
